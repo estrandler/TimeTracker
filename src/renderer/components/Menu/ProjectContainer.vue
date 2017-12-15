c77b4198ddca9dc1da585dfca04f67ccf942e02a
Anton Elgh <anton.elgh@avanade.com>
2017-12-15 17:57:05 +0100

    fixed spaces

--- src/renderer/components/Menu/ProjectContainer.vue
+++ src/renderer/components/Menu/ProjectContainer.vue
@@ -1,19 +1,11 @@
 <template lang="pug">
-    .project-container
-        .project-button
-            h1 1
-        .project-button
-            h1 2
-        .project-button
-            h1 3
-        .project-button
-            h1 4
+    //- .project-container
+    //-     .project-button
+    //-         h1 1
+    //-     .project-button
+    //-         h1 2
+    //-     .project-button
+    //-         h1 3
+    //-     .project-button
+    //-         h1 4
 </template>
-
-
-<div class="project-container">
-<div class="project-button"><h1>1</h1></div>
-<div class="project-button"><h1>2</h1></div>
-<div class="project-button"><h1>3</h1></div>
-<div class="project-button"><h1>4</h1></div>
-</div>
--- src/renderer/components/Menu/StartAndStop.vue
+++ src/renderer/components/Menu/StartAndStop.vue
@@ -16,7 +16,7 @@
 </script>
 
 <style lang="scss">
-    .stop-button{
+  .stop-button{
 
-    }
+  }
 </style>

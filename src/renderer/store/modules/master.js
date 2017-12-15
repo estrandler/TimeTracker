1d723f6889dca4bf03060acf5864988208bdc144
Anton Gunnarsson <anton.gunnarsson@avanade.com>
2017-12-15 17:21:32 +0100

    Add start and stop period actions

--- src/renderer/store/modules/master.js
+++ src/renderer/store/modules/master.js
@@ -9,7 +9,7 @@ const state = {
     {
       startTime: '2017-01-01 00:00:00',
       endTime: null,
-      project: null
+      project: 'project1'
     }
   ]
 }
@@ -17,14 +17,29 @@ const state = {
 const getters = {}
 
 const mutations = {
-  [types.PROJECT_ADD] (state, projectName) {
+  [types.PROJECT_ADD](state, projectName) {
     state.projects.push(projectName)
+  },
+  [types.PROJECT_PERIOD_START](state, projectName) {
+    state.periods.push({ startTime: new Date(), endTime: null, project: projectName })
+  },
+  [types.PROJECT_PERIOD_STOP](state, projectName) {
+    state.periods
+      .filter(p => p.project === projectName)
+      .filter(p => p.endTime === null)
+      .map(p => p.endTime = new Date())
   }
 }
 
 const actions = {
   addProject ({ commit }, projectName) {
     commit(types.PROJECT_ADD, projectName)
+  },
+  startPeriod ({ commit }, projectName) {
+    commit(types.PROJECT_PERIOD_START, projectName)
+  },
+  stopPeriod ({ commit}, projectName) {
+    commit(types.PROJECT_PERIOD_STOP, projectName)
   }
 }
 

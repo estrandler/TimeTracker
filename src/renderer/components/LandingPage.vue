6892dd613a4b2194140510ea4cd82e44d7ba311b
Anton Gunnarsson <anton.gunnarsson@avanade.com>
2017-12-15 16:20:37 +0100

    Convert LandingPage to scss

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -21,7 +21,7 @@
   }
 </script>
 
-<style>
+<style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
   * {
@@ -53,9 +53,9 @@
   main {
     display: flex;
     justify-content: space-between;
+    
+    > div { flex-basis: 50%; }
   }
-
-  main > div { flex-basis: 50%; }
 
   .left-side {
     display: flex;
@@ -73,34 +73,36 @@
     font-size: 20px;
     font-weight: bold;
     margin-bottom: 6px;
+
+    &.alt {
+      font-size: 18px;
+      margin-bottom: 10px;
+    }
   }
 
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
+  .doc {
+    p {
+      color: black;
+      margin-bottom: 10px;
+    }
 
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
+    button {
+      font-size: .8em;
+      cursor: pointer;
+      outline: none;
+      padding: 0.75em 2em;
+      border-radius: 2em;
+      display: inline-block;
+      color: #fff;
+      background-color: #4fc08d;
+      transition: all 0.15s ease;
+      box-sizing: border-box;
+      border: 1px solid #4fc08d;
 
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+      &.alt {
+        color: #42b983;
+        background-color: transparent;
+      }
+    }
   }
 </style>
